9e6b2caa1b7e88f26f1acf51a8654dbb9a8d139f
马利康 <41456389+pathfindGulf@users.noreply.github.com>
2020-06-09 18:14:02 +0800

    代码生成

---
+++ src/main/java/com/ikingtech/framework/web/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.ikingtech.framework.web.controller;
+
+
+/**
+ * web层通用数据处理
+ *
+ * @author iking
+ */
+public class BaseController {
+}
---
+++ src/main/java/com/mlk/tools/codeGenerator/CodeGenerator.java
@@ -0,0 +1,138 @@
+package com.mlk.tools.codeGenerator;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @功能描述: 代码生成工具；修改配置后执行即可；
+ *      查询所有表名 SELECT GROUP_CONCAT( table_name ) FROM information_schema.TABLES WHERE table_schema = '你的DATA_BASE'
+ * @author: mlk
+ * @data: 2020/6/9
+ */
+public class CodeGenerator {
+
+    // 数据源配置
+    private static String HOST = "127.0.0.1";
+    private static String PORT = "3306";
+    private static String DATA_BASE = "measure";
+    private static String USER_NAME = "root";
+    private static String PASSWORD = "123456";
+    private static String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("mlk");
+        gc.setFileOverride(true);//覆盖文件
+//        gc.setIdType(IdType.AUTO); //自增：AUTO ； uuid：ASSIGN_UUID
+        gc.setSwagger2(true); //实体属性 Swagger2 注解
+        gc.setDateType(DateType.ONLY_DATE); //时间类型
+//        gc.setEnableCache(true); //是否在xml中添加二级缓存配置
+        gc.setBaseResultMap(true); //xml
+        gc.setBaseColumnList(true); //xml
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://" + HOST + ":" + PORT + "/" + DATA_BASE + "?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC&serverTimezone=GMT%2B8");
+        dsc.setDriverName(DRIVER_NAME);
+        dsc.setUsername(USER_NAME);
+        dsc.setPassword(PASSWORD);
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(scanner("模块名"));
+        pc.setParent("com.ikingtech.test");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setChainModel(true); //链式模型
+        strategy.setEntityTableFieldAnnotationEnable(true);//实体字段注解
+//        strategy.setSuperEntityColumns("id");// 写于父类中的公共字段
+        strategy.setSuperControllerClass("com.ikingtech.framework.web.controller.BaseController");// 公共父类Controller
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
